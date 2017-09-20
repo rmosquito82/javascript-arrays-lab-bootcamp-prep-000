4697b1471896669be78b62635789cf9f4ae77251
r_lito <rui.mosquito@gmail.com>
2017-09-20 21:23:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  var newArray = kittens.push(name)
+  var newArray = kittens.splice(-1, 0, name)
   return newArray
 }
